82b89dbbe8d4ff9fb85627832a7464ac114bc3a1
lagmaral <lagmaral@uoc.edu>
2024-12-22 18:15:49 +0100

    animiacion

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,3 +1,4 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
@@ -7,7 +8,23 @@ import { MatHeaderCellDef, MatCellDef } from '@angular/material/table';
   standalone: true,
   imports: [CommonModule,MatTableModule, MatHeaderCellDef, MatCellDef],
   templateUrl: './grid.component.html',
-  styleUrl: './grid.component.scss'
+  styleUrl: './grid.component.scss',
+  animations: [
+    trigger('tableAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(20px)' }),
+            stagger('100ms', [
+              animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class GridComponent {
   @Input() data: any[] = [];
